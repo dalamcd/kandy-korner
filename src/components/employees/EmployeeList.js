ce8a0f9917143f718c0cbf529f87aa1abe99c9ce
dalamcd <dalamcd@gmail.com>
2020-12-01 15:41:44 -0600

    Fixed bug

--- src/components/employees/EmployeeList.js
+++ src/components/employees/EmployeeList.js
@@ -10,11 +10,8 @@ export const EmployeeList = props => {
 
 	useEffect(() => {
 		getLocations()
+		.then(getEmployees);
 	}, [])
-
-	useEffect(() => {
-		getEmployees()
-	}, [locations])
 
 	return (
 		<>
